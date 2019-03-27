ae6d9ad6a2534152a528abfa4f03bbd69e6b351a
alabliuk <andrelabliuk@gmail.com>
2019-03-26 23:59:11 -0300

    csharp-parte-2 Aula 2 Tipos

--- Desenvolvedor .NET/02_Introdução à Orientação a Objetos/source/ByteBank/01-ByteBank/Program.cs
+++ Desenvolvedor .NET/02_Introdução à Orientação a Objetos/source/ByteBank/01-ByteBank/Program.cs
@@ -6,7 +6,18 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente primeiraContaCorrente = new ContaCorrente();
+            primeiraContaCorrente.saldo = 200;
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            primeiraContaCorrente.saldo += 100;
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            ContaCorrente segundaContaCorrente = new ContaCorrente();
+            segundaContaCorrente.saldo = 50;
+
+            Console.WriteLine("primeira conta tem " + primeiraContaCorrente.saldo);
+            Console.WriteLine("segunda conta tem " + segundaContaCorrente.saldo);
 
             Console.ReadLine();
         }
