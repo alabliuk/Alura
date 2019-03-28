41dbe19fb1788210456d121ad2dd8c9c3c534c14
T_AndreL1 <andre.leme@verifone.com>
2019-03-28 18:53:23 -0300

    Aula 02 - Adicionando alunos na agenda

---
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/FormularioActivity.java
@@ -0,0 +1,26 @@
+package com.example.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        Button botaoSalvar =  findViewById(R.id.formulario_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Botão clicado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
