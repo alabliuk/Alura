de8d71e9d653a86c06fb17141c2371fb9da3f5df
T_AndreL1 <andre.leme@verifone.com>
2019-03-29 19:47:59 -0300

    Aula 03 - Utilizando menus

--- Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/FormularioActivity.java
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/FormularioActivity.java
@@ -1,7 +1,11 @@
 package com.example.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,15 +16,24 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+    }
 
-        Button botaoSalvar =  findViewById(R.id.formulario_salvar);
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FormularioActivity.this, "Botão clicado", Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "Aluno Salvo", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
 
+        return super.onOptionsItemSelected(item);
     }
 }
--- Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/MainActivity.java
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
+        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe","Daniel", "Ronaldo", "Jeferson", "Felipe","Daniel", "Ronaldo", "Jeferson", "Felipe","Daniel", "Ronaldo", "Jeferson", "Felipe"};
         ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
 
         ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+
+        Button novoAluno = findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentVaiProFormulario = new Intent(MainActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
 
     }
 }
