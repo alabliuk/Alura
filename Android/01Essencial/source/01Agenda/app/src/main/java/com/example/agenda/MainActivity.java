ab44ebe02d9cf73002482c0032892f731764c488
alabliuk <andrelabliuk@gmail.com>
2019-04-09 00:10:01 -0300

    Mudando o layout do aplicativo

--- Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/MainActivity.java
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.agenda.adapter.AlunosAdapter;
 import com.example.agenda.dao.AlunoDAO;
 import com.example.agenda.modelo.Aluno;
 
@@ -63,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         List<Aluno> alunos = dao.buscaAlunos();
         dao.close();
 
-        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        AlunosAdapter adapter = new AlunosAdapter(this, alunos);
         listaAlunos.setAdapter(adapter);
     }
 
---
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/adapter/AlunosAdapter.java
@@ -0,0 +1,45 @@
+package com.example.agenda.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.agenda.modelo.Aluno;
+
+import java.util.List;
+
+public class AlunosAdapter extends BaseAdapter {
+
+    private final List<Aluno> alunos;
+    private final Context context;
+
+    public AlunosAdapter(Context context, List<Aluno> alunos) {
+        this.context = context;
+        this.alunos = alunos;
+    }
+
+    @Override
+    public int getCount() {
+        return alunos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return alunos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return alunos.get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView view = new TextView(context);
+        Aluno aluno = alunos.get(position);
+        view.setText(aluno.toString());
+        return view;
+    }
+}
