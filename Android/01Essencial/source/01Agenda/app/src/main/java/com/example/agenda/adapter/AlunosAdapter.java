1a195734b501edf952c2a8b2dc04506058ecd3f6
AndreL1 <andre.leme@verifone.com>
2019-04-09 11:33:19 -0300

    Melhorias no layout

--- Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/adapter/AlunosAdapter.java
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/adapter/AlunosAdapter.java
@@ -1,11 +1,16 @@
 package com.example.agenda.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.agenda.R;
 import com.example.agenda.modelo.Aluno;
 
 import java.util.List;
@@ -37,9 +42,30 @@ public class AlunosAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView view = new TextView(context);
         Aluno aluno = alunos.get(position);
-        view.setText(aluno.toString());
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = convertView;
+        if(convertView == null){
+            view = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView campoNome = view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = view.findViewById(R.id.item_foto);
+        String caminhoFoto = aluno.getCaminhoFoto();
+
+        if (caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+
         return view;
     }
 }
