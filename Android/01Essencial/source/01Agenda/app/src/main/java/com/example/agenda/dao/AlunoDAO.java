90d01c12141c8bac4be00cdada2845cf4bb234cc
AndreL1 <andre.leme@verifone.com>
2019-04-11 20:26:37 -0300

    Verificando se o SMS foi enviado por um aluno

--- Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/dao/AlunoDAO.java
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/dao/AlunoDAO.java
@@ -87,4 +87,12 @@ public class AlunoDAO extends SQLiteOpenHelper {
         String[] params = {aluno.getId().toString()};
         db.update("Alunos", dados, "id = ?", params);
     }
+
+    public boolean ehAluno(String telefone) {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM Alunos WHERE telefone = ?", new String[]{telefone});
+        int resultados = c.getCount();
+        c.close();
+        return resultados > 0;
+    }
 }
--- Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/receiver/SMSReceiver.java
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/receiver/SMSReceiver.java
@@ -3,12 +3,35 @@ package com.example.agenda.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
+
+import com.example.agenda.R;
+import com.example.agenda.dao.AlunoDAO;
 
 public class SMSReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Chegou um SMS", Toast.LENGTH_SHORT).show();
 
+        Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
+        byte[] pdu = (byte[]) pdus[0];
+        String formato = (String) intent.getSerializableExtra("format");
+
+        SmsMessage sms = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            sms = SmsMessage.createFromPdu(pdu, formato);
+        } else {
+            sms = SmsMessage.createFromPdu(pdu);
+        }
+
+        String telefone = sms.getDisplayOriginatingAddress();
+        AlunoDAO dao = new AlunoDAO(context);
+        if(dao.ehAluno(telefone)){
+            Toast.makeText(context, "Chegou um SMS de Aluno!", Toast.LENGTH_SHORT).show();
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.msg);
+            mp.start();
+        }
+        dao.close();
     }
 }
