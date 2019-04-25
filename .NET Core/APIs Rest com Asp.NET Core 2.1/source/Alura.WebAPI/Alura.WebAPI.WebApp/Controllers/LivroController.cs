0742032b3b0863ce0caec936f8623331c64ec281
alabliuk <andrelabliuk@gmail.com>
2019-04-25 00:35:15 -0300

    Integrando com outros sistemas

--- .NET Core/APIs Rest com Asp.NET Core 2.1/source/Alura.WebAPI/Alura.WebAPI.WebApp/Controllers/LivroController.cs
+++ .NET Core/APIs Rest com Asp.NET Core 2.1/source/Alura.WebAPI/Alura.WebAPI.WebApp/Controllers/LivroController.cs
@@ -59,6 +59,17 @@ namespace Alura.ListaLeitura.WebApp.Controllers
             return View(model.ToModel());
         }
 
+        [HttpGet]
+        public IActionResult DetalhesSemHtml(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return Json(model.ToModel());
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Detalhes(LivroUpload model)
