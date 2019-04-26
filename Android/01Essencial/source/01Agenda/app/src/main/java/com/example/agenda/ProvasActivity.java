db842043b8d8c64b868ec2a0bbec1a0d4dacd29c
AndreL1 <andre.leme@verifone.com>
2019-04-26 18:51:16 -0300

    Usando o Fragment

--- Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/ProvasActivity.java
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/ProvasActivity.java
@@ -1,6 +1,8 @@
 package com.example.agenda;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +13,6 @@ import android.widget.Toast;
 
 import com.example.agenda.modelo.Prova;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,28 +23,9 @@ public class ProvasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provas);
 
-        List<String> topicosPort = Arrays.asList("Sujeito", "Objetos direto", "Objeto indireto");
-        Prova provaPortugues = new Prova("Portugues", "23/04/2019", topicosPort);
-
-        List<String> topicosMat = Arrays.asList("Equaçoes", "Trigonometria");
-        Prova provaMatematica = new Prova("Matematica", "25/04/2019", topicosMat);
-
-        List<Prova> provas = Arrays.asList(provaPortugues, provaMatematica);
-
-        ArrayAdapter<Prova> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, provas);
-
-        ListView lista = findViewById(R.id.provas_lista);
-        lista.setAdapter(adapter);
-
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Prova prova = (Prova) parent.getItemAtPosition(position);
-                Toast.makeText(ProvasActivity.this, "Clicou na prova de " + prova, Toast.LENGTH_SHORT).show();
-                Intent vaiParaDetalhes = new Intent(ProvasActivity.this, DetalhesProvaActivity.class);
-                vaiParaDetalhes.putExtra("prova", prova);
-                startActivity(vaiParaDetalhes);
-            }
-        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction tx = fragmentManager.beginTransaction();
+        tx.replace(R.id.frame_principal, new ListaProvasFragment());
+        tx.commit();
     }
 }
