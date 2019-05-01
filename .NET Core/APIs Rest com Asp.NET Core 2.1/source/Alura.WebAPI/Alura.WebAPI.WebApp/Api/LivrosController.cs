f5ed4f8f6cec50f1f2738c7fa0234f8e3483e617
alabliuk <andrelabliuk@gmail.com>
2019-05-01 00:18:09 -0300

    Implementando as outras operações do CRUD

--- .NET Core/APIs Rest com Asp.NET Core 2.1/source/Alura.WebAPI/Alura.WebAPI.WebApp/Api/LivrosController.cs
+++ .NET Core/APIs Rest com Asp.NET Core 2.1/source/Alura.WebAPI/Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -21,11 +21,55 @@ namespace Alura.WebAPI.WebApp.Api
         public IActionResult Recuperar(int id)
         {
             var model = _repo.Find(id);
-            if(model == null)
+            if (model == null)
             {
                 return NotFound();
             }
             return Json(model.ToModel());
+        }
+
+        [HttpPost]
+        public IActionResult Incluir(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+                var uri = Url.Action("Recuperar", new { id = livro.Id });
+                return Created(uri, livro);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repo.Alterar(livro);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public IActionResult Remover(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repo.Excluir(model);
+            return NoContent();
         }
     }
 }
