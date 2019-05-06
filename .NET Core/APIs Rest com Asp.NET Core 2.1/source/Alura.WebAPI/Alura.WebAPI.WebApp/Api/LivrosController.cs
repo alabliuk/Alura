08f4c42c4294c49686b28cf769b870ae2f8eb6f6
AndreL1 <andre.leme@verifone.com>
2019-05-06 19:31:22 -0300

    Protegendo nossa API

--- .NET Core/APIs Rest com Asp.NET Core 2.1/source/Alura.WebAPI/Alura.WebAPI.WebApp/Api/LivrosController.cs
+++ .NET Core/APIs Rest com Asp.NET Core 2.1/source/Alura.WebAPI/Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Linq;
 using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Alura.WebAPI.WebApp.Api
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class LivrosController : ControllerBase
