465980e6d7f24dc39048b59f89d781612c614315
alabliuk <andrelabliuk@gmail.com>
2019-05-12 04:17:16 -0300

    Customização do mapa

--- Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/MapaActivity.java
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/MapaActivity.java
@@ -16,7 +16,7 @@ public class MapaActivity extends AppCompatActivity {
 
         FragmentManager manager =  getSupportFragmentManager();
         FragmentTransaction tx = manager.beginTransaction();
-        tx.replace(R.id.frame_mapa, new SupportMapFragment());
+        tx.replace(R.id.frame_mapa, new MapaFragment());
         tx.commit();
     }
 }
---
+++ Android/01Essencial/source/01Agenda/app/src/main/java/com/example/agenda/MapaFragment.java
@@ -0,0 +1,63 @@
+package com.example.agenda;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+
+import com.example.agenda.dao.AlunoDAO;
+import com.example.agenda.modelo.Aluno;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapaFragment extends SupportMapFragment implements OnMapReadyCallback {
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng posicao = pegaCoordenadaDoEndereco("Rua Vergueiro 3185, Vila Mariana, São Paulo");
+        if(posicao != null) {
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(posicao, 17);
+            googleMap.moveCamera(update);
+        }
+
+        AlunoDAO alunoDAO = new AlunoDAO(getContext());
+        for(Aluno aluno: alunoDAO.buscaAlunos()){
+            LatLng coordenada = pegaCoordenadaDoEndereco(aluno.getEndereco());
+            if(coordenada != null){
+                MarkerOptions marcador = new MarkerOptions();
+                marcador.position(coordenada);
+                marcador.title(aluno.getNome());
+                marcador.snippet(String.valueOf(aluno.getNota()));
+                googleMap.addMarker(marcador);
+            }
+        }
+        alunoDAO.close();
+    }
+
+    private LatLng pegaCoordenadaDoEndereco(String endereco) {
+        try {
+            Geocoder geocoder = new Geocoder(getContext());
+            List<Address> resultados = geocoder.getFromLocationName(endereco, 1);
+            if (!resultados.isEmpty()) {
+                LatLng posicao = new LatLng(resultados.get(0).getLatitude(), resultados.get(0).getLongitude());
+                return posicao;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
