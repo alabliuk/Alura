3bbc628b68c153c9687b9ecd1412a0c807da0046
alabliuk <andrelabliuk@gmail.com>
2019-05-12 14:06:40 -0300

    Adicionando Retrofit no projeto

---
+++ Android/Android com Web Service/source/android-sync-parte-1-projeto-inicial/app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
@@ -0,0 +1,14 @@
+package br.com.alura.agenda.retrofit;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+public class RetrofitInicializador {
+
+    private final Retrofit retrofit;
+
+    public RetrofitInicializador() {
+        retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.187:8080/api/")
+                .addConverterFactory(JacksonConverterFactory.create()).build();
+    }
+}
