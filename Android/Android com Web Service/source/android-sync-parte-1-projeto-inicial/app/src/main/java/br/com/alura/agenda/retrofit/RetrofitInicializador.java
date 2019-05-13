e46f95341de7b8a705d6d470e177d20ba233ecef
alabliuk <andrelabliuk@gmail.com>
2019-05-13 01:23:20 -0300

    Adicionando o logging interceptor

--- Android/Android com Web Service/source/android-sync-parte-1-projeto-inicial/app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
+++ Android/Android com Web Service/source/android-sync-parte-1-projeto-inicial/app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
@@ -1,6 +1,8 @@
 package br.com.alura.agenda.retrofit;
 
 import br.com.alura.agenda.services.AlunoService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
@@ -9,8 +11,17 @@ public class RetrofitInicializador {
     private final Retrofit retrofit;
 
     public RetrofitInicializador() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        client.addInterceptor(interceptor);
+
         retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.187:8080/api/")
-                .addConverterFactory(JacksonConverterFactory.create()).build();
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(client.build())
+                .build();
     }
 
     public AlunoService getAlunoService() {
