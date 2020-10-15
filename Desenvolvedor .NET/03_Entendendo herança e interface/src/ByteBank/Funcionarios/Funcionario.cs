c6942e1a2369aa40128615306bbcbf990c9ec061
alabliuk <andrelabliuk@gmail.com>
2020-10-15 01:52:32 -0300

    Iniciando o estudo de herança e interface c#

---
+++ Desenvolvedor .NET/03_Entendendo herança e interface/src/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        // 0 - funcionário
+        // 1 - diretor
+        // 2 - designer
+        // N - ...
+        private readonly int _tipo;
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+        
+        // construtor
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+                return Salario;
+
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ Desenvolvedor .NET/03_Entendendo herança e interface/src/ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using ByteBank.Funcionarios;
+using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+        }
+    }
+}
