3290de7a07473f4261a5d393b29a90daea961111
alabliuk <andrelabliuk@gmail.com>
2021-02-04 21:18:56 -0300

    Iniciando estudo do framework de teste xUnit

---
+++ Desenvolvedor .NET/Testes de unidade e TDD com xUnit/Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(fulano, 990);
+
+            leilao.TerminaPregao();
+
+            Console.WriteLine(leilao.Ganhador.Valor);
+        }
+    }
+}
---
+++ Desenvolvedor .NET/Testes de unidade e TDD com xUnit/Alura.LeilaoOnline/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
